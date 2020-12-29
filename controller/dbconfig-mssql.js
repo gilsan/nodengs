a8edcf3abeffd3f557ed87a5b4bdbaa235b02338
kyebyungha <kbh@exciform.com>
2020-12-29 17:54:57 +0900

    dbconfig-mmssql.js 커밋

---
+++ controller/dbconfig-mssql.js
@@ -0,0 +1,11 @@
+module.exports = {
+	user: 'ngs',
+	password: 'ngs12#$',
+	server: '112.169.53.30',
+	port :  4306,
+	database: 'ngs_data',
+ 	options: {
+		encrypt: false,
+		enableArithAbort: true
+	},
+};
