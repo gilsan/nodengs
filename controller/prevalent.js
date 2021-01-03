ef6888ac1f89ad6dd77313cf354b1b1e123f97e0
gilsan <jhhan@allaboutsip.co.kr>
2021-01-04 02:28:47 +0900

    Update prevalent.js
    logger 추가

--- controller/prevalent.js
+++ controller/prevalent.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mssql = require('mssql');
+const logger = require('../common/winston');
+
 const config = {
     user: 'ngs',
     password: 'ngs12#$',
@@ -23,40 +25,41 @@ const poolConnect = pool.connect();
 const  prevalentInsertHandler = async (pathologyNum, prevalent) => {
     await poolConnect; // ensures that the pool has been created
     let result;
-    console.log('[29][prevalentdata]', pathologyNum, prevalent);
+    logger.info('[28][prevalentdata insert]pathologyNum=' + pathologyNum);
+    logger.info('[29][prevalentdata insert]prevalent=' + prevalent);
 
     let sql = "delete from prevalent where  pathologyNum = @pathologyNum ";
+    logger.info("[32][prevalentdata insert]sql=" + sql);
+
     try {
         const request = pool.request()
             .input('pathologyNum', mssql.VarChar, pathologyNum); 
             
         const result = await request.query(sql)       
         //return result;
-      } catch (err) {
-        console.error('SQL error', err);
+      } catch (error) {
+        logger.error('[41][prevalentdata delete]error=' + error.message);
     } 
     
     const len = prevalent.length;
-
 
     if ( len > 0 ) {
        prevalent.forEach( async(item) => {
         const prevalent = item;
         const qry = "insert into prevalent (pathologyNum, prevalent) values(@pathologyNum, @prevalent)"; 
+        logger.info('[41][prevalentdata insert]qry=' + qry);
 
         try {
             const request = pool.request()
             .input('pathologyNum', mssql.VarChar, pathologyNum)
             .input('prevalent', mssql.VarChar, prevalent);
             result = await request.query(qry);
-        } catch (err) {
-            console.error('SQL error', err);
+        } catch (error) {
+            logger.error('[58][prevalentdata insert]error=' + error.message);
         }
        });
-
     }
  
-
     return result;
 }
 
@@ -65,42 +68,53 @@ exports.prevalentdata = (req, res, next) => {
     const pathologyNum = req.body.pathologyNum;
     const prevalent = req.body.prevalent;
 
+    logger.info('[71][prevalentdata ]pathologyNum=' + pathologyNum);
+    logger.info('[71][prevalentdata ]prevalent=' + prevalent);
+
     const result = prevalentInsertHandler(pathologyNum, prevalent);
     result.then(data => {
    
         console.log('[72][prevalentdata]', data);
           res.json({message:'SUCCESS'});
      })
-     .catch( err  => res.sendStatus(500));
+     .catch( error  => {
+        logger.error('[80][prevalentdata]error=' + error.message);
+        res.sendStatus(500)
+    });
 
  };
-
 
  const  prevalentSelectHandler = async (pathologyNum) => {
     await poolConnect; // ensures that the pool has been created
 
-    	//insert Query 생성
-    const qry = "select prevalent  from prevalent   where pathologyNum = @pathologyNum ";
+    logger.info('[89][prevalentdata select]pathologyNum=' + pathologyNum);
+
+    //select Query 생성
+    const qry = "select prevalent from prevalent where pathologyNum = @pathologyNum ";
+    logger.info('[91][prevalentdata select]sql=' + qry);
     
     try {
-
         const request = pool.request()
             .input('pathologyNum', mssql.VarChar, pathologyNum);
 
         const result = await request.query(qry);
         return result.recordset; 
-    }catch (err) {
-            console.error('SQL error', err);
-        }
-  }
+    }catch (error) {
+        logger.error('[102][prevalentdata select]error=' + error.message);
+    }
+}
 
-  exports.prevalentList = (req, res, next) => {
+exports.prevalentList = (req, res, next) => {
     const pathologyNum = req.body.pathologyNum;
+
+    logger.error('[109][prevalentlist]pathologyNum=' + pathologyNum);
     const result = prevalentSelectHandler(pathologyNum);
     result.then(data => {  
           console.log('[100][prevalentList]', data);
           res.send(data.map(item => item.prevalent));
      })
-     .catch( err  => res.sendStatus(500));; 
-
+     .catch( error  => {
+        logger.error('[116][prevalentlist]error=' + error.message);
+        res.sendStatus(500);
+    })
  };
