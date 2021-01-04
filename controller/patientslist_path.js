df7037d401e1b7f17bee2f9db6d8587c8fdce942
gilsan <jhhan@allaboutsip.co.kr>
2021-01-04 16:30:21 +0900

    Update patientslist_path.js
    병리 환자 검색 수정

--- controller/patientslist_path.js
+++ controller/patientslist_path.js
@@ -80,7 +80,45 @@ function nvl(st, defaultStr){
 const  messageHandler = async (today) => {
     await poolConnect; // ensures that the pool has been created
    
-    const sql ="select * from [dbo].[patientinfo_path] where left(prescription_date, 8) = '" + today + "'";
+    const sql ="select isnull(FLT3ITD, '') FLT3ITD, \
+    isnull(accept_date, '') accept_date, \
+    isnull(age, '') age, \
+    isnull(appoint_doc, '') appoint_doc, \
+    isnull(bamFilename, '') bamFilename, \
+    isnull(createDate, '') createDate, \
+    isnull(dna_rna_ext, '') dna_rna_ext, \
+    isnull(examin, '') examin, \
+    isnull(gender, '') gender, \
+    isnull(id, '') id, \
+    isnull(irpath, '') irpath, \
+    isnull(key_block, '') key_block, \
+    isnull(management, '') management, \
+    isnull(msiscore, '') msiscore, \
+    isnull(name, '') name, \
+    isnull(organ, '') organ, \
+    isnull(orpath, '')  orpath, \
+    isnull(pathological_dx, '') pathological_dx, \
+    isnull(pathology_num, '') pathology_num, \
+    isnull(patientID, '') patientID, \
+    isnull(prescription_code, '') prescription_code, \
+    isnull(prescription_date, '') prescription_date, \
+    isnull(prescription_no, '') prescription_no, \
+    isnull(recheck, '') recheck, \
+    isnull(rel_pathology_num, '') rel_pathology_num, \
+    isnull(report_date, '') report_date, \
+    isnull(screenstatus, '') screenstatus, \
+    isnull(sendEMR, '') sendEMR, \
+    isnull(sendEMRDate, '') sendEMRDate, \
+    isnull(test_code, '') test_code, \
+    isnull(tsvFilteredDate, '') tsvFilteredDate, \
+    isnull(tsvFilteredFilename, '') tsvFilteredFilename, \
+    isnull(tsvFilteredStatus, '') tsvFilteredStatus, \
+    isnull(tsvirfilename, '') tsvirfilename, \
+    isnull(tsvorfilename, '') tsvorfilename, \
+    isnull(tumor_cell_per, '') tumor_cell_per, \
+    isnull(tumor_type, '') tumor_type, \
+    isnull(tumorburden, '') tumorburden, \
+    isnull(worker, '') worker  from [dbo].[patientinfo_path] where left(prescription_date, 8) = '" + today + "'";
     logger.info("[81][patientinfo_path select]sql=" + sql);
     try {
         const request = pool.request(); // or: new sql.Request(pool1)
@@ -115,7 +153,45 @@ const messageHandler2 = async (start, end, patientID, pathology_num) => {
     logger.info("[112][patientinfo_path select]start=" + start);
     logger.info("[112][patientinfo_path select]end=" + end);
     
-    let sql = "select * from [dbo].[patientinfo_path] \
+    let sql = "select isnull(FLT3ITD, '') FLT3ITD, \
+    isnull(accept_date, '') accept_date, \
+    isnull(age, '') age, \
+    isnull(appoint_doc, '') appoint_doc, \
+    isnull(bamFilename, '') bamFilename, \
+    isnull(createDate, '') createDate, \
+    isnull(dna_rna_ext, '') dna_rna_ext, \
+    isnull(examin, '') examin, \
+    isnull(gender, '') gender, \
+    isnull(id, '') id, \
+    isnull(irpath, '') irpath, \
+    isnull(key_block, '') key_block, \
+    isnull(management, '') management, \
+    isnull(msiscore, '') msiscore, \
+    isnull(name, '') name, \
+    isnull(organ, '') organ, \
+    isnull(orpath, '')  orpath, \
+    isnull(pathological_dx, '') pathological_dx, \
+    isnull(pathology_num, '') pathology_num, \
+    isnull(patientID, '') patientID, \
+    isnull(prescription_code, '') prescription_code, \
+    isnull(prescription_date, '') prescription_date, \
+    isnull(prescription_no, '') prescription_no, \
+    isnull(recheck, '') recheck, \
+    isnull(rel_pathology_num, '') rel_pathology_num, \
+    isnull(report_date, '') report_date, \
+    isnull(screenstatus, '') screenstatus, \
+    isnull(sendEMR, '') sendEMR, \
+    isnull(sendEMRDate, '') sendEMRDate, \
+    isnull(test_code, '') test_code, \
+    isnull(tsvFilteredDate, '') tsvFilteredDate, \
+    isnull(tsvFilteredFilename, '') tsvFilteredFilename, \
+    isnull(tsvFilteredStatus, '') tsvFilteredStatus, \
+    isnull(tsvirfilename, '') tsvirfilename, \
+    isnull(tsvorfilename, '') tsvorfilename, \
+    isnull(tumor_cell_per, '') tumor_cell_per, \
+    isnull(tumor_type, '') tumor_type, \
+    isnull(tumorburden, '') tumorburden, \
+    isnull(worker, '') worker  from [dbo].[patientinfo_path] \
                where left(prescription_date, 8) >= '" + start + "' \
                and left(prescription_date, 8) <= '" + end + "' ";
 
