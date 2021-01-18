fcd4767eb52036aeeaf2ce2d010f92512aa4bc8c
gilsan <jhhan@allaboutsip.co.kr>
2021-01-18 22:04:52 +0900

    EMR 이력 통계 update

--- controller/manageStatisticsMapper.js
+++ controller/manageStatisticsMapper.js
@@ -11,6 +11,7 @@ const listHandler = async (req) => {
 	const userNm			= req.body.userNm; 
 	const startDay			= req.body.startDay; 
 	const endDay			= req.body.endDay;
+	const dept			    = req.body.dept;
 	
   
 	let sql =" select a.seq, a.patientID, a.name, substring(a.accept_date,1,4) + '-' + " ;
@@ -19,7 +20,7 @@ const listHandler = async (req) => {
 	sql = sql +"  from stat_Log  a  " ;
 	sql = sql +"  inner join  users b " ;
 	sql = sql +" on a.sender = b.user_id  " ; 
-	sql = sql + " where 1 = 1 ";
+	sql = sql + " where dept = @dept ";
 	if(userId != "") 
 		sql = sql + " and a.patientID like '%"+userId+"%'";
 	if(userNm != "") 
@@ -38,7 +39,8 @@ const listHandler = async (req) => {
          .input('userId', mssql.VarChar, userId)
 		 .input('userNm', mssql.VarChar, userNm) 
 		 .input('startDay', mssql.VarChar, startDay) 
-		 .input('endDay', mssql.VarChar, endDay)   ; 
+		 .input('endDay', mssql.VarChar, endDay) 
+		 .input('dept', mssql.VarChar, dept)   ; 
        const result = await request.query(sql) 
        return result.recordset;
    } catch (err) {
