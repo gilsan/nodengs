d2874d4daa9304cae0f37c986d5531ed2010e696
gilsan <jhhan@allaboutsip.co.kr>
2021-01-25 06:21:56 +0900

    logger update

--- controller/registeruser.js
+++ controller/registeruser.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
+const logger = require('../common/winston');
 const mssql = require('mssql');
 const dbConfigMssql = require('../common/dbconfig.js');
 
 const pool = new mssql.ConnectionPool(dbConfigMssql);
 const poolConnect = pool.connect();
- 
 
 const  listHandler = async (user_id) => {
   await poolConnect;   
+
+  logger.info("[14][registeruser list]user_id=" + user_id );
   const sql= `select id, user_id  from users \
          where user_id = @user_id`; 
-
-	console.error('SQL-->', sql);	
+  logger.info("[17][registeruser SQL=" + sql);	
 
   try {
       const request = pool.request()
@@ -22,16 +23,14 @@ const  listHandler = async (user_id) => {
       console.dir(result);
       const data = result.recordset[0];
       return data;
-  } catch (err) {
-      console.error('SQL error', err);
+  } catch (error) {
+    logger.error('[27][registeruser list]err=' + error.message);
   }
 }
-    
 
+// insert user
 const  insertHandler = async (req) => {
-  await poolConnect;   
-
-console.log(req.body);
+  await poolConnect;  
 
   const user_id     = req.body.user_id;
   
@@ -39,20 +38,24 @@ console.log(req.body);
   const user_nm     = req.body.user_nm;
   const dept		= req.body.dept;
   const part		= req.body.part;
+
+  logger.info('[42][registeruser insert]user_id=' + user_id + ', password=' + password
+                      + ', user_nm=' + user_nm + ', dept=' + dept + ', part=' + part);
  
   const sql = "insert into users ( "
 			  +"	user_id		"
 			  +"	,password	"
-			  +"	,user_nm		"
-			  +"	,user_gb		"
+			  +"	,user_nm	"
+			  +"	,user_gb	"
 			  +"	,dept		"
 			  +"	,reg_date	"
 			  +"	,part		"
 			  +"	,approved ) " 
 			  +" values( "
 			  +" @user_id, @password, @user_nm, 'U',  @dept "
-			  +" , getdate(),  @part, 'N') "; 
-       
+        +" , getdate(),  @part, 'N') "; 
+  logger.info("[57][registeruser insert]sql=" + sql);
+
   try {
       const request = pool.request()
         .input('user_id', mssql.VarChar, user_id)
@@ -64,14 +67,14 @@ console.log(req.body);
       console.dir( result);
       
       return result;
-  } catch (err) {
-      console.error('SQL error', err);
+  } catch (error) {
+    logger.error("[71][registeruser insert]error="  + error.message);
   }
 }
 
 exports.register = (req,res, next) => {
   
-  console.log(req.body);
+  logger.info("[77][registeruser ]req=" + JSON.stringify(req.body));
 
   const user_id    = req.body.user_id;  
   const result  = listHandler(user_id);
@@ -85,8 +88,14 @@ exports.register = (req,res, next) => {
 				console.log('register->>>>>>step2');
 				res.json({message: 'success'});
 			})
-			.catch( err  => res.sendStatus(500)); 
+			.catch( error =>{
+        logger.error("[92][registeruser insert]err=" + error.message);
+        res.sendStatus(500);
+      }); 
 		}   
-  }).catch( err  => res.sendStatus(500)); 
+  }).catch( error => {
+    logger.error("[97][registeruser list]err=" + error.message);
+    res.sendStatus(500)
+  }); 
 
 }
