b03116027010eaa1d5a1130e8dde47ffdc170bc1
gilsan <jhhan@allaboutsip.co.kr>
2021-02-02 17:01:55 +0900

    nu update

--- routes/report_patient.js
+++ routes/report_patient.js
@@ -4,8 +4,8 @@ const router = express.Router();
  const reportPatientController = require('../controller/report_patient');
 
 // 검진자 리스트
-router.get('/list', reportPatientController.getList);
-router.get('/count', reportPatientController.getCount);
-router.get('/insert', reportPatientController.insetList);
+router.post('/list', reportPatientController.getList);
+router.post('/count', reportPatientController.getCount);
+router.post('/insert', reportPatientController.insetList);
 
 module.exports = router;
