ed1dfd3449ae309b690560f3d03766513a58deba
gilsan <jhhan@allaboutsip.co.kr>
2021-02-15 21:26:14 +0900

    router update

--- routes/emrroute.js
+++ routes/emrroute.js
@@ -1,12 +1,19 @@
+// 진검 emr 전송
+
 const app = require('express');
 const querystring = require('querystring');
 const router = app.Router();
 const axios = require('axios');
- 
+const logger = require('../common/winston');
+
+// 교육
 let url = 'http://emr012edu.cmcnu.or.kr/cmcnu/.live';
+//운영
+// let url = 'http://emr012.cmcnu.or.kr/cmcnu/.live';
  
 router.post('/redirectEMR', (req, res, next) => {  
    console.log(req.query.data);
+   logger.info ('[웹에서 받은 데이타]url=' + url);
    // const data = req.params.data;
    //  const submit_id   = req.params.submit_id;
    //  const business_id = req.params.business_id;
@@ -29,16 +36,15 @@ router.post('/redirectEMR', (req, res, next) => {
    // const url = req.query.data;
    // urlData = escape(url)
     console.log('[웹에서 받은 데이타]', req.body);
-    res.json({msg:'SUCCESS'});
-   // axios({
-   //     method: 'post',
-   //     url ,
-   //     data: querystring.stringify(req.body) 
-   // })
-   // .then(function(response) {
-   //    res.json(response.data)
-   //    console.log('[ response from EMR ] \n  ', response);      
-   // });
+   axios({
+       method: 'post',
+       url ,
+       data: querystring.stringify(req.body) 
+   })
+   .then(function(response) {
+      res.json(response.data)
+      console.log('[ response from EMR ] \n  ', response);      
+   });
    
 });
 
--- routes/pathologyRoute.js
+++ routes/pathologyRoute.js
@@ -1,26 +1,29 @@
+
+// 병리 emr 전송
 const app = require('express');
 const querystring = require('querystring');
 const router = app.Router();
 const axios = require('axios');
+const logger = require('../common/winston');
 
 // 교육
-let url = 'http://emr012edu.cmcnu.or.kr/cmcnu/.live';
-// 운영 
-//let url = 'http://emr012.cmcnu.or.kr/cmcnu/.live';
+//let url = 'http://emr012edu.cmcnu.or.kr/cmcnu/.live';
+//운영
+let url = 'http://emr012.cmcnu.or.kr/cmcnu/.live';
  
 router.post('/redirectEMR', (req, res, next) => {  
    console.log('[웹에서 받은 데이타]', req.body);
-   res.json({msg: 'SUCCESS'});
-   // 임시사용
-   // axios({
-   //     method: 'post',
-   //     url,
-   //     data: querystring.stringify(req.body) 
-   // })
-   // .then(function(response) {
-   //    res.json(response.data)
-   //    console.log('[ response from EMR ] \n  ', response.data);     
-   // });
+   logger.info ('[웹에서 받은 데이타]url=' + url);
+   //logger.info ('[웹에서 받은 데이타]data=' + req.body);
+   axios({
+       method: 'post',
+       url,
+       data: querystring.stringify(req.body) 
+   })
+   .then(function(response) {
+      res.json(response.data)
+      console.log('[ response from EMR ] \n  ', response.data);     
+   });
    
 });
 
