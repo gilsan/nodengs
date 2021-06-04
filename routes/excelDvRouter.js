81621e4a89ba876024a40bd60307cce30a0c0813
gilsan <jhhan@allaboutsip.co.kr>
2021-06-04 14:32:59 +0900

    bug fix

--- routes/excelDvRouter.js
+++ routes/excelDvRouter.js
@@ -6,6 +6,6 @@ const excelDvController = require('../controller/excelDv');
 
 // 검진자 excel 리스트
 router.post('/insert', excelDvController.excelDvSave);
-router.post('/list', excelDvController.excelDvList);
+router.get('/list', excelDvController.excelDvList);
 
 module.exports = router;
