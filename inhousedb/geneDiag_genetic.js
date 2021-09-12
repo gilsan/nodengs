766ff3df6e60061513eeca1a9c1c6b2b2270dc91
gilsan <jhhan@allaboutsip.co.kr>
2021-09-13 01:37:09 +0900

    gene update

---
+++ inhousedb/geneDiag_genetic.js
@@ -0,0 +1,134 @@
+const fs = require('fs');
+const logger = require('../common/winston');
+
+const mssql = require('mssql');
+const dbConfigMssql = require('../common/dbconfig.js');
+const pool = new mssql.ConnectionPool(dbConfigMssql);
+const poolConnect = pool.connect();
+
+// 특수문자찿기 변경하기
+function findChar(findChar) {
+  const check = findChar.toString().indexOf(',');
+  let result = '';
+
+	if(check === -1) {
+		result = findChar;
+	} else {
+        result = findChar.replace( /,/gi, '\,');
+	}
+
+	return result;
+}
+
+   
+/**
+ * 문자열이 빈 문자열인지 체크하여 기본 문자열로 리턴한다.
+ * @param st           : 체크할 문자열
+ * @param defaultStr    : 문자열이 비어있을경우 리턴할 기본 문자열
+ */
+function nvl(st, defaultStr){
+    
+  //console.log('st=', st);
+  if(st === undefined || st == null || st == "") {
+      st = defaultStr ;
+  }
+      
+  return st ;
+}
+
+function parse_tsv(s, f) {
+  s = s.replace(/,/g, ";");
+  var ix_end = 0;
+  for (var ix = 0; ix < s.length; ix = ix_end + 1) {
+    ix_end = s.indexOf('\n', ix);
+    if (ix_end == -1) {
+      ix_end = s.length;
+    }
+  
+    var row = s.substring(ix, ix_end).split('\t');
+
+    f(row);
+  }
+}
+
+function loadData(filePath) {
+   if (fs.existsSync(filePath)) {
+     var tsvData = fs.readFileSync(filePath, 'utf-8');
+     var rowCount = 0;
+     var scenarios = [];
+     parse_tsv(tsvData, (row) => {
+       rowCount++;
+       if (rowCount >= 0) {
+         scenarios.push(row);
+       }
+     });
+     return scenarios;
+   } else {
+     return [];
+   }
+}
+  
+var tsvData = '../inhouseupload/genetic.txt';
+var rowCount = 0;
+
+var rowData = loadData(tsvData);
+
+rowData.forEach ( async (row, index) =>  {
+
+  await poolConnect;
+  rowCount++;
+  console.log(rowCount);
+  if (rowCount >= 0) {
+
+    logger.info('length =' + row.length);
+
+      var type = 'genetic';
+
+      var test_code = nvl(row[0].replace( /"/gi, ''), "");
+      logger.info('[84][genediag]test_code=' + test_code);
+
+      if (test_code != "") {
+
+        for (var i= 1; i < row.length -1; i ++ ) {
+        var temp_data = nvl(row[i].replace( /"/gi, ''), "");
+        var gene = findChar(temp_data);   
+
+        if (gene != "") {
+          logger.info('[87][genediag]gene=' + gene);
+          
+          
+            const sql =`insert_genediag_genetic` ;
+
+            try {
+                
+                const request = pool.request()
+                  .input('gene', mssql.VarChar, gene)
+                  .input('type', mssql.NVarChar, type) 
+                  .input('test_code', mssql.NVarChar, test_code) 
+                  .output('TOTALCNT', mssql.int, 0);
+                
+              let result =  '';
+              await request.execute(sql, (err, recordset, returnValue) => {
+                  if (err)
+                  {
+                    console.log ("172[genediag]err message=" + err.message);
+                  }
+
+                  console.log("[175][genediag]recordset="+ recordset);
+                  console.log("[176][genediag]returnValue="+ returnValue);
+
+                  result = returnValue;
+                  console.log("[179]result=" + JSON.stringify(result));
+                }); 
+
+                console.log("result=", result);
+            
+          } catch(err) {
+              logger.error('[185][genediag]err=' + err.message);
+          } 
+          
+        }
+      }
+    }
+  } 
+});// end of if loop
