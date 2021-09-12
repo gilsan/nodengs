65e58f4c07b7b290ddd1dbed66bb7a949808e2cc
gilsan <jhhan@allaboutsip.co.kr>
2021-09-12 22:49:09 +0900

    diaggene fix

--- controller/diaggene.js
+++ controller/diaggene.js
@@ -13,16 +13,36 @@ const e = require('express');
 const pool = new mssql.ConnectionPool(dbConfigMssql);
 const poolConnect = pool.connect();
 
+/**
+ * 문자열이 빈 문자열인지 체크하여 기본 문자열로 리턴한다.
+ * @param st           : 체크할 문자열
+ * @param defaultStr    : 문자열이 비어있을경우 리턴할 기본 문자열
+ */
+function nvl(st, defaultStr){
+    
+    //console.log('st=', st);
+    if(st === undefined || st == null || st == "") {
+        st = defaultStr ;
+    }
+        
+    return st ;
+}
+
 // 진검 유전자 목록출력
-const listHandler = async (type) => {
+const listHandler = async (type, test_code) => {
     await poolConnect;
 
-    logger.info('[20][diaggene]type=' + type);
-    const sql ="select isnull(gene, '') gene  from genediag where type =@type";
+    let test_cd = nvl(test_code, '');
+
+    logger.info('[20][diaggene]type=' + type + ', test_code=' + test_cd) ;
+    let sql =`select isnull(gene, '') gene  from genediag where type ='` + type + `'`;
+    if (test_cd != '') {
+        sql = sql + ` and test_code = '` + test_cd + `'`;
+    }
+
     logger.info('[20][diaggene]sql=' + sql);
     try {
         const request = pool.request()
-          .input('type', mssql.VarChar, type); 
           const result = await request.query(sql) 
           return result.recordset;
     } catch(error) {
@@ -35,8 +55,9 @@ exports.listDiagGene = (req,res, next) => {
 
     logger.info('[32]listDiagGene req=' + JSON.stringify( req.body)); 
     const type = req.body.type;
+    const test_code = req.body.test_code;
 
-    const result = listHandler(type);
+    const result = listHandler(type, test_code);
     result.then(data => {
         res.json(data);
     })
@@ -47,15 +68,21 @@ exports.listDiagGene = (req,res, next) => {
 }
 
 // 진검 유전자 목록 입력
-const inserHandler = async (type, gene) => {
+const inserHandler = async (type, gene, test_code) => {
     await poolConnect;
     let count;
-    const sql = 'select count(*) as cnt  from genediag where type=@type and gene=@gene';
+    let test_cd = nvl(test_code, '');
+
+    const sql = `select count(*) as cnt  from genediag
+                     where type=@type 
+                     and gene=@gene 
+                     and test_code= @test_cd`;
     logger.info('[65][diaggene controller query ]query=' + sql);
     try {
         const request = pool.request()
             .input('gene',mssql.VarChar, gene)
-            .input('type', mssql.VarChar, type);
+            .input('type', mssql.NVarChar, type)
+            .input('test_cd', mssql.VarChar, test_cd);
         const result = await request.query(sql);
              
         count = result.recordset[0].cnt;
@@ -65,12 +92,13 @@ const inserHandler = async (type, gene) => {
     }
 
     if ( count === 0 ) {
-        const qry = 'insert into genediag (gene, type) values(@gene, @type)';
+        const qry = 'insert into genediag (gene, type, test_code) values(@gene, @type, @test_cd)';
         logger.info('[66][DiagGene controller]insert sql=' + sql); 
         try {
             const request = pool.request()
             .input('gene',mssql.VarChar, gene)
-            .input('type', mssql.VarChar, type);
+            .input('type', mssql.VarChar, type)
+            .input('test_cd', mssql.VarChar, test_cd);
             const result = await request.query(qry);
             return result;   
         } catch(error) {
@@ -84,8 +112,9 @@ exports.insertDiagGene = (req,res, next) => {
     logger.info('[81]insertDiagGene req=' + JSON.stringify(req.body));
     const type = req.body.type;
     const gene = req.body.gene;
+    const test_code = req.body.test_code;
     
-    const result = inserHandler(type, gene);
+    const result = inserHandler(type, gene, test_code);
     result.then(data => {
         res.json({ message: 'SUCCESS'});
     })
@@ -96,17 +125,23 @@ exports.insertDiagGene = (req,res, next) => {
 }
 
 // 진검 유전자 변경
-const updateHandler = async (type, gene, newgene) => {
+const updateHandler = async (type, gene, newgene, test_code) => {
     await poolConnect;
+    let test_cd = nvl(test_code, '');
   
-    logger.info('[100]updateDiagGene controller data=' + type + ", " + gene + ", " + newgene);
-    const sql = 'update genediag set gene=@newgene where type=@type and gene=@gene';
+    logger.info('[100]updateDiagGene controller data=' + type + ", " + gene + ", " + newgene + ", " + test_cd);
+    const sql = `update genediag
+                     set gene=@newgene 
+                     where type=@type 
+                     and gene=@gene 
+                     and test_code= @test_cd`;
     logger.info('[l02][diaggene controller update ]query=' + sql);
     try {
         const request = pool.request()
              .input('type',mssql.VarChar, type)
              .input('newgene', mssql.VarChar, newgene)
-             .input('gene',mssql.VarChar, gene);           
+             .input('gene',mssql.VarChar, gene)
+             .input('test_cd',mssql.VarChar, test_cd);           
              const result = await request.query(sql);
              return result;
     } catch (error) {
@@ -121,7 +156,8 @@ exports.updateDiagGene = (req,res, next) => {
     const type = req.body.type; 
     const gene = req.body.gene;
     const newgene = req.body.newgene;
-    const result = updateHandler(type, gene, newgene);
+    const test_code = req.body.test_code;
+    const result = updateHandler(type, gene, newgene, test_code);
     result.then(data => {
            res.json({ message: 'SUCCESS'});
     })
@@ -132,16 +168,18 @@ exports.updateDiagGene = (req,res, next) => {
 }
 
 // 진검 유전자 삭제
-const deleteHandler = async (type, gene) => {
+const deleteHandler = async (type, gene, test_code) => {
     await poolConnect;
+    let test_cd = nvl(test_code, '');
   
-    logger.info('[137]updateDiagGene controller data=' + type + ", " + gene );
+    logger.info('[137]updateDiagGene controller data=' + type + ", " + gene + "," + test_cd );
     const sql = ' delete  from genediag where type=@type and gene=@gene';
     logger.info('[ll4][diaggene controller delete ]query=' + sql);
     try {
         const request = pool.request()
              .input('gene', mssql.VarChar, gene)
-             .input('type',mssql.VarChar, type);
+             .input('type',mssql.VarChar, type)
+             .input('test_cd',mssql.VarChar, test_cd);
             
         const result = await request.query(sql);
         return result;
@@ -157,7 +195,8 @@ exports.deleteDiagGene = (req,res, next) => {
     
     const gene = req.body.gene;
     const type = req.body.type; 
-    const result = deleteHandler(type,gene);
+    const test_code = req.body.test_code;
+    const result = deleteHandler(type,gene, test_code);
     result.then(data => {
            res.json({ message: 'SUCCESS'});
     })
@@ -170,7 +209,7 @@ exports.deleteDiagGene = (req,res, next) => {
 const listAllHandler = async () => {
     await poolConnect;
 
-    const sql ="select isnull(gene, '') gene, type  from genediag ";
+    const sql ="select isnull(gene, '') gene, type , isnull(test_code, '') test_code from genediag ";
     logger.info('[173][list diaggene]sql=' + sql);
     try {
         const request = pool.request(); 
@@ -196,17 +235,22 @@ exports.listAllDiagGene = (req,res, next) => {
 }
 
 // 중복검사
-const checkHandler = async (type, gene) => {
+const checkHandler = async (type, gene, test_code) => {
   await poolConnect;
   
   logger.info('[201]count DiagGene controller data=' + type + ", " + gene );
-  const sql = "select count(*) as count from genediag where type=@type and gene=@gene";
+  const sql = `select count(*) as count 
+                    from genediag 
+                    where type=@type 
+                    and gene=@gene
+                    and test_code=@test_cd`;
   logger.info('[293]count DiagGene controller sql=' + sql );
 
     try {
         const request = pool.request()
              .input('type',mssql.VarChar, type)
-             .input('gene',mssql.VarChar, gene);           
+             .input('gene',mssql.VarChar, gene)
+             .input('test_cd',mssql.VarChar, test_cd);           
              const result = await request.query(sql);
              return result.recordsets[0];
     } catch (error) {
@@ -219,7 +263,8 @@ exports.duplicateGene = (req, res, next) => {
 
     const gene = req.body.gene;
     const type = req.body.type;
-    const result = checkHandler(type, gene);
+    const test_code = req.body.test_code;    
+    const result = checkHandler(type, gene, test_code);
     result.then(data => {
         res.json(data);
     })
