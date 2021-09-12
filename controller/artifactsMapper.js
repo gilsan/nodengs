9b6a9584d3a8ee651c69038fbc3e3aeb53893e73
gilsan <jhhan@allaboutsip.co.kr>
2021-09-12 15:27:08 +0900

    inhouse update

--- controller/artifactsMapper.js
+++ controller/artifactsMapper.js
@@ -6,11 +6,28 @@ const dbConfigMssql = require('../common/dbconfig.js');
 const pool = new mssql.ConnectionPool(dbConfigMssql);
 const poolConnect = pool.connect(); 
 
+/**
+ * 문자열이 빈 문자열인지 체크하여 기본 문자열로 리턴한다.
+ * @param st           : 체크할 문자열
+ * @param defaultStr    : 문자열이 비어있을경우 리턴할 기본 문자열
+ */
+function nvl(st, defaultStr){
+    
+    //console.log('st=', st);
+    if(st === undefined || st == null || st == "") {
+        st = defaultStr ;
+    }
+        
+    return st ;
+}
+
 const listHandler = async (req) => {
     await poolConnect;  
     const genes			= req.body.genes;  
     const coding		= req.body.coding; 
-    logger.info('[12]artifcats listHandler genes=' + genes + ", coding=" + coding);
+    let sheet =  nvl(req.body.sheet, "");
+
+    logger.info('[12]artifcats listHandler genes=' + genes + ", coding=" + coding + ", sheet=" + sheet);
 	
 	let sql ="select id, genes, location, exon, transcript, coding, amino_acid_change ";
     sql = sql + " from artifacts ";
@@ -19,6 +36,42 @@ const listHandler = async (req) => {
 		sql = sql + " and genes like '%"+genes+"%'";
     if(coding != "") 
         sql = sql + " and coding like '%"+coding+"%'";
+
+    if(sheet.length > 0 )
+    {
+        if (sheet == 'AMLALL') {
+            sql = sql +  " and test_code in ('LPE471', 'LPE545')";
+        } else if (sheet == 'MDSMPN') {  // MDS/MPN 
+            sql = sql +  " and test_code in ('LPE473')";
+        } else if (sheet == '악성림프종') {  // 악성림프종
+            sql = sql +  " and test_code in ('LPE474', 'LPE475')";
+        } else if (sheet == '유전성유전질환') {  // 유전성유전질환
+            sql = sql +   ` and test_code in ('LPE548', 'LPE439', 'LPE452', 'LPE453', 'LPE454', 'LPE455', 
+                                    'LPE456', 'LPE488', 'LPE489', 'LPE490', 'LPE497', 'LPE498', 'LPE499',
+                                    'LPE517', 'LPE518', 'LPE519', 'LPE520', 'LPE521', 'LPE522', 'LPE523',
+                                    'LPE524', 'LPE525', 'LPE526', 'LPE527', 'LPE528', 'LPE529', 'LPE530',
+                                    'LPE531', 'LPE532', 'LPE533', 'LPE534', 'LPE535', 'LPE536', 'LPE537',
+                                    'LPE538', 'LPE539', 'LPE540', 'LPE541', 'LPE542', 'LPE543')`;
+        } else if (sheet == 'Sequencing') {  // Sequencing
+            sql = sql +   ` and test_code in ('LPC100', 'LPC101', 'LPC117', 'LPC118', 'LPC194',
+                                    'LPE115', 'LPE141', 'LPE156', 'LPE194', 'LPE221', 'LPE227', 'LPE229',
+                                    'LPE231', 'LPE233', 'LPE236', 'LPE237', 'LPE238', 
+                                    'LPE241', 'LPE242', 'LPE243', 'LPE245', 'LPE247', 'LPE249',
+                                    'LPE251', 'LPE258', 'LPE262', 'LPE267', 'LPE272', 'LPE276', 
+                                    'LPE280', 'LPE282', 'LPE285', 'LPE287', 'LPE289', 'LPE290', 'LPE295',
+                                    'LPE302', 'LPE306', 'LPE308', 'LPE310', 'LPE313', 'LPE316', 'LPE320',
+                                    'LPE334', 'LPE337', 'LPE340', 'LPE341', 'LPE342', 'LPE343', 'LPE349', 
+                                    'LPE352', 'LPE354', 'LPE356', 'LPE358', 'LPE360', 'LPE362', 'LPE364', 
+                                    'LPE366', 'LPE368', 'LPE371', 'LPE374', 'LPE375', 'LPE378', 'LPE379', 
+                                    'LPE384', 'LPE391', 'LPE392', 'LPE410', 'LPE412', 'LPE414', 'LPE418', 
+                                    'LPE420', 'LPE428', 'LPE431', 'LPE433', 'LPE436', 'LPE457', 'LPE460', 
+                                    'LPE462', 'LPE469', 'LPE477', 'LPE482', 'LPE494', 'LPE495') `
+        } else if (sheet == 'MLPA') {  // MLPA
+            sql = sql +  ` and test_code in ('LPE232', 'LPE294', 'LPE322', 'LPE332', 'LPE351', 'LPE369', 
+                                    'LPE377', 'LPE464')`;
+        } 
+    }
+    
     sql = sql + " order by id ";
 
 	logger.info('[12]artifcats listHandler sql' + sql);
