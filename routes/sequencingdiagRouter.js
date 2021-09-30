e02d73df25dca0556cb9383e09ce5439b99d11f4
gilsan <jhhan@allaboutsip.co.kr>
2021-09-30 19:18:16 +0900

    commit

--- routes/sequencingdiagRouter.js
+++ routes/sequencingdiagRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
  const sequencingController = require('../controller/sequencingdiag');
 
 // 병리 sequencing
- router.post('/list',  sequencingController.listequencing);
+ router.get('/list',  sequencingController.listequencing);
 router.post('/insert', sequencingController.insertsequencing);
  
 module.exports = router;
