79800f643d19e455c60a0c97ce6dbba07c40898b
gilsan <jhhan001@gmail.com>
2021-12-28 17:46:03 +0900

    bug fix

--- controller/patientslist_diag.js
+++ controller/patientslist_diag.js
@@ -311,15 +311,15 @@ const  messageHandler2 = async (start, end, patientID, specimenNo, sheet, status
                                     'LPE531', 'LPE532', 'LPE533', 'LPE534', 'LPE535', 'LPE536', 'LPE537',
                                     'LPE538', 'LPE539', 'LPE540', 'LPE541', 'LPE542', 'LPE543')`;
         } else if (sheet_1 == 'Sequencing') {  // Sequencing
-            sql = sql +   ` and test_code in ('LPC100', 'LPC101', 'LPC117', 'LPC118', 'LPC194',   'LPE115', 'LPE141', 'LPE156', 'LPE194', 'LPE221',
+            sql = sql +   ` and test_code in ('LPC100', 'LPC101', 'LPC117', 'LPC118', 'LPC194', 'LPE115', 'LPE141', 'LPE156', 'LPE194', 'LPE221',
             'LPE227', 'LPE229', 'LPE231', 'LPE233', 'LPE236', 'LPE237', 'LPE238', 'LPE241', 'LPE243', 'LPE245',
             'LPE247', 'LPE249', 'LPE251', 'LPE258', 'LPE262', 'LPE267', 'LPE272', 'LPE275', 'LPE276', 'LPE280',
-            'LPE282', 'LPE285', 'LPE287', 'LPE289',  'LPE290',  'LPE295', 'LPE302', 'LPE306', 'LPE308', 'LPE310',
-            'LPE313', 'LPE314', 'LPE316', 'LPE320', 'LPE334', 'LPE337', 'LPE340', 'LPE341', 'LPE342', 'LPE343',
-            'LPE349', 'LPE352', 'LPE354', 'LPE356', 'LPE358', 'LPE360', 'LPE362', 'LPE364', 'LPE366', 'LPE371',
-            'LPE374', 'LPE375', 'LPE378', 'LPE379', 'LPE384', 'LPE391', 'LPE392', 'LPE368', 'LPE410', 'LPE412',
+            'LPE282', 'LPE285', 'LPE287', 'LPE289', 'LPE290', 'LPE295', 'LPE302', 'LPE306', 'LPE308', 'LPE310',
+            'LPE313', 'LPE314', 'LPE316', 'LPE320', 'LPE330', 'LPE334', 'LPE337', 'LPE340', 'LPE341', 'LPE342', 'LPE343',
+            'LPE349', 'LPE352', 'LPE354', 'LPE356', 'LPE358', 'LPE360', 'LPE362', 'LPE364', 'LPE366', 'LPE368', 'LPE371',
+            'LPE374', 'LPE375', 'LPE378', 'LPE379', 'LPE384', 'LPE391', 'LPE392', 'LPE410', 'LPE412',
             'LPE414', 'LPE418', 'LPE420', 'LPE428', 'LPE431', 'LPE433', 'LPE436', 'LPE457', 'LPE460', 'LPE462',
-            'LPE469', 'LPE477', 'LPE481', 'LPE482', 'LPE494', 'LPE495', 'LPE330', 'LPE438') `
+            'LPE469', 'LPE477', 'LPE481', 'LPE482', 'LPE494', 'LPE495', 'LPE438') `
         } else if (sheet_1 == 'MLPA') {  // MLPA
             sql = sql +  ` and test_code in ('LPE232', 'LPE294', 'LPE322', 'LPE332', 'LPE351', 'LPE369', 
                                     'LPE377', 'LPE464')`;
